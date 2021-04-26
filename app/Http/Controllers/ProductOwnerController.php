7d37107d57a704504fc54ab5f01ed6a276ab83e1
bc2mighty <akinadetunmise8@gmail.com>
2021-04-26 22:06:47 +0100

    Started Model, Controller and Migration set up

---
+++ app/Http/Controllers/ProductOwnerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductOwner;
+use Illuminate\Http\Request;
+
+class ProductOwnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductOwner  $productOwner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductOwner $productOwner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductOwner  $productOwner
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductOwner $productOwner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductOwner  $productOwner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductOwner $productOwner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductOwner  $productOwner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductOwner $productOwner)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProductPhotographController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductPhotograph;
+use Illuminate\Http\Request;
+
+class ProductPhotographController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductPhotograph  $productPhotograph
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductPhotograph $productPhotograph)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductPhotograph  $productPhotograph
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductPhotograph $productPhotograph)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductPhotograph  $productPhotograph
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductPhotograph $productPhotograph)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductPhotograph  $productPhotograph
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductPhotograph $productPhotograph)
+    {
+        //
+    }
+}
