c98be3dcafdaa517cd29ba8c9978eb4932aaf1b5
bc2mighty <akinadetunmise8@gmail.com>
2021-04-28 10:03:15 +0100

    Route COmment

--- routes/api.php
+++ routes/api.php
@@ -23,25 +23,35 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // Photographer Routes
 Route::group(['prefix' => 'photographer'], function (){
+    // Account Creation
     Route::post('/create', [PhotographerController::class, 'store']);
+    // Account Update
     Route::put('/{photographer}', [PhotographerController::class, 'update']);
+    // Post Pictures taken for products in processing facilities
     Route::post('/{photographer}/product/{product}', [PhotographerController::class, 'pictures']);
+    // Account Login
     Route::post('/login', [PhotographerController::class, 'login']);
 });
 
 // Prouct Owners' Routes
 Route::group(['prefix' => 'productOwner'], function (){
+    // Account Creation
     Route::post('/create', [ProductOwnerController::class, 'store']);
+    // Account Update
     Route::put('/{productOwner}', [ProductOwnerController::class, 'update']);
+    // Account Login
     Route::post('/login', [ProductOwnerController::class, 'login']);
+    // Send Product To Processing Facility
     Route::put('/{productOwner}/{product}', [ProductOwnerController::class, 'product']);
+    // Get Created Products
     Route::get('/{productOwner}/products', [ProductOwnerController::class, 'products']);
+    // Get Thumbnails of Product Photographs that are not yet approved
     Route::get('/{productOwner}/products/photographs/unapproved', [ProductOwnerController::class, 'unapproved']);
+    // Get Thumbnails and high Resolution Photographs of Products that were approved
     Route::get('/{productOwner}/products/photographs/approved', [ProductOwnerController::class, 'approved']);
-    Route::post('/{productOwner}/productPhotograph/{productPhotograph}/approve', 
-        [ProductOwnerController::class, 'approve'
-    ]);
+    // Approve/Disapprove Product Photographs
+    Route::post('/{productOwner}/productPhotograph/{productPhotograph}/approve', [ProductOwnerController::class, 'approve']);
 });
 
-// Prouct Routes
+// Prouct Routes CRUD
 Route::resource('products', ProductController::class);
